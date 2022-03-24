d695fee68f29612244f8e90a75f9657c747de789
willvelida <willvelida@hotmail.co.uk>
2022-03-24 20:38:08 +1300

    Fixing function, adding ARM template and README for Cosmos MI Sample

---
+++ cosmosdb-function-managed-identity/README.md
@@ -0,0 +1,3 @@
+# Using Managed Identities to authenticate with Azure Cosmos DB in Azure Functions
+
+This sample shows you how you can use System-Assigned Managed Identities to authenitcate calls between Azure Cosmos DB and Azure Functions.
--- cosmosdb-function-managed-identity/src/Todo.Api/Todo.Api/Models/TodoItem.cs
+++ cosmosdb-function-managed-identity/src/Todo.Api/Todo.Api/Models/TodoItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace Todo.Api.Models
 {
     public class TodoItem
     {
+        [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
         public string Title { get; set; }
         public bool IsComplete { get; set; }
